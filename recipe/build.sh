e369f9ea053a10e780ca36e6e6b5bf5719490a8a
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2024-08-27 18:01:10 -0700

    Fix build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,5 +7,5 @@ cmake -GNinja ${CMAKE_ARGS} \
     -DCMAKE_PREFIX_PATH="${PREFIX}" \
     -DCMAKE_BUILD_TYPE=Release \
     ..
-cmake --build . --verbose --config Release -- -v -j %CPU_COUNT%
+cmake --build . --verbose --config Release -- -v -j ${CPU_COUNT}
 cmake --install . --verbose --config Release
